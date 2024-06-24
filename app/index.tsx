0ca6144781f11762864aaf6c9edfdbee8f274bb8
tako0614 <shoutatomiyama0614@gmail.com>
2024-06-25 04:43:44 +0900

    na

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
